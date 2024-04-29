e5ad674e856de5da8cf0a3a75a0addd549e18710
Yeoni Rhee <yeonirhee@yeonis-mbp.wifi.local.cmu.edu>
2024-04-29 15:01:03 -0400

    Final Sotopia Task Submission

--- sotopia_space/benchmark.py
+++ sotopia_space/benchmark.py
@@ -1,7 +1,7 @@
 import gradio as gr # type: ignore
 import pandas as pd
 from sotopia_space.constants import MODEL_OPTIONS
-from sotopia_space.utils import apply_length_penalty 
+from sotopia_space.utils import post_processing
 
 LP_MODE = "v2"
 original_df, ablation_df = None, None
@@ -16,13 +16,15 @@ def benchmark_table():
                 
     with gr.TabItem("Vs GPT-3.5", elem_id="od-benchmark-tab-table-ablation", id=0, elem_classes="subtab"):
         original_df = pd.read_json('data_dir/models_vs_gpt35.jsonl', lines=True)
-        default_main_df = apply_length_penalty(original_df, ablation_df, length_penalty=DEFAULT_LP, mode=LP_MODE, LP_original_dfs=LP_original_dfs) 
+        default_main_df = original_df
         default_main_df = default_main_df.sort_values(by="GOAL [0, 10]", ascending=False)
+        default_main_df = post_processing(default_main_df, None)
         # add a Rank column to the first columnn (starting from 1)
         default_main_df.insert(0, "Rank", range(1, 1 + len(default_main_df)))
+        
         with gr.Row():
             with gr.Column(scale=4):
-                gr.Markdown("**Vs GPT3.5**: The interlocutors are compared against GPT-3.5, the baseline model.")
+                gr.Markdown("<h3>**Vs GPT3.5**: The interlocutors are compared against GPT-3.5, the baseline model.")
         TYPES = ["number", "markdown", "number"]
         leaderboard_table = gr.components.Dataframe(
             value=default_main_df,
--- sotopia_space/utils.py
+++ sotopia_space/utils.py
@@ -1,3 +1,39 @@
+from ui_constants import column_names
+from sotopia_space.constants import MODEL_INFO
 
-def apply_length_penalty(original_df, ablation_df, length_penalty=0.2, mode='v1', LP_original_dfs=None):
-    return original_df 
+
+def make_clickable_model(model_name):
+    global MODEL_INFO
+    if model_name in MODEL_INFO:
+        if MODEL_INFO[model_name]["hf_model_id"].startswith("http"):
+            link = MODEL_INFO[model_name]["hf_model_id"]
+            return f'🔒 <a target="_blank" href="{link}" style="color: var(--link-text-color); text-decoration: underline;text-decoration-style: dotted;">{MODEL_INFO[model_name]["pretty_name"]}</a>'
+        else:
+            link = f"https://huggingface.co/{MODEL_INFO[model_name]['hf_model_id']}"
+            return f'🔥 <a target="_blank" href="{link}" style="color: var(--link-text-color); text-decoration: underline;text-decoration-style: dotted;">{MODEL_INFO[model_name]["pretty_name"]}</a>'
+    else:
+        return model_name
+    
+# Formats the columns
+def formatter(x):
+    if type(x) is str:
+        x = x
+    else: 
+        x = round(x, 2)
+    return x
+
+def post_processing(df, model_len_info):
+    if model_len_info:
+        df["Length"] = df["model_name"].apply(lambda x: model_len_info[x]["avg_len"])
+
+    for col in df.columns:
+        if col == "model_name":
+            df[col] = df[col].apply(lambda x: x.replace(x, make_clickable_model(x)))
+        else:
+            df[col] = df[col].apply(formatter) # For numerical values 
+    df.rename(columns=column_names, inplace=True)
+    df.sort_values(by="GOAL [0, 10]", inplace=True, ascending=False)
+    # put the "Overall Elo" and "Task-Avg Elo" column to the front
+    # add the length info
+    df = df[["model_name", "GOAL [0, 10]"] + [col for col in df.columns if col not in ["model_name", "GOAL [0, 10]"]]]
+    return df
